1d54516cf0335b3620614aace89f132af690e03b
Joey Mukherjee <joey@swri.edu>
2020-08-20 08:25:46 -0500

    Added daily/overall change

--- lib/models/portfolio/folder.dart
+++ lib/models/portfolio/folder.dart
@@ -1,16 +1,21 @@
 import 'package:meta/meta.dart';
 
+class FolderChange {
+  final double change;
+  final double changePercentage;
+
+  FolderChange ({this.change = 0.0, this.changePercentage = 0.0});
+}
+
 class PortfolioFolderModel {
   final String name;
   final bool exclude;
-  final double change;
-  final double changePercentage;
+  final FolderChange daily = FolderChange ();
+  final FolderChange overall = FolderChange ();
 
   PortfolioFolderModel({
     @required this.name,
     @required this.exclude,
-    this.change = 0.0,
-    this.changePercentage = 0.0
   });
 
   factory PortfolioFolderModel.fromJson(Map<String, dynamic> json) {
