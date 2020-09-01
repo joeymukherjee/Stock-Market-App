a71beb544cbe1c2c9109ded75e927f8d8e4e9ccd
Joey Mukherjee <joey@swri.edu>
2020-09-01 18:48:40 -0500

    Added device picker to test on iPad looking device

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sentry/sentry.dart';
 import 'package:sma/shared/styles.dart';
 
 import 'package:sma/bloc/news/news_bloc.dart';
@@ -14,7 +15,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:sma/widgets/about/about.dart';
 import 'package:sma/widgets/home.dart';
-
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
@@ -26,46 +27,51 @@ void main() async {
   bool initialTheme = prefs.getBool('theme') == null ? true : prefs.getBool('theme');
 
   runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider<SettingsBloc>(
-          create: (context) => SettingsBloc(initialTheme)
-        ),
-        BlocProvider<WatchlistBloc>(
-          create: (context) => WatchlistBloc(),
-        ),
-        BlocProvider<PortfolioFoldersBloc>(
-          create: (context) => PortfolioFoldersBloc(),
-        ),
-        BlocProvider<ProfileBloc>(
-          create: (context) => ProfileBloc(),
-        ),
-        BlocProvider<SearchBloc>(
-          create: (context) => SearchBloc(),
-        ),
-        BlocProvider<SectorPerformanceBloc>(
-          create: (context) => SectorPerformanceBloc(),
-        ),
-        BlocProvider<NewsBloc>(
-          create: (context) => NewsBloc(),
-        ),
-        BlocProvider<TradesBloc>(
-          create: (context) => TradesBloc(),
-        ),
-      ],
-      child: BlocBuilder <SettingsBloc, SettingsState> (
-        builder: (context, state) {
-          return MaterialApp(
-            title: 'Stonks',
-            theme: state.isNight ? darkMode : lightMode,
-            home: StockMarketAppHome(),
-            debugShowCheckedModeBanner: true,
-            routes: {
-              '/about': (context) => AboutSection(),
-            },
-          );
-        }
-      )
+    DevicePreview(
+      enabled: false,
+      builder: (_) => MultiBlocProvider(
+        providers: [
+          BlocProvider<SettingsBloc>(
+            create: (context) => SettingsBloc(initialTheme)
+          ),
+          BlocProvider<WatchlistBloc>(
+            create: (context) => WatchlistBloc(),
+          ),
+          BlocProvider<PortfolioFoldersBloc>(
+            create: (context) => PortfolioFoldersBloc(),
+          ),
+          BlocProvider<ProfileBloc>(
+            create: (context) => ProfileBloc(),
+          ),
+          BlocProvider<SearchBloc>(
+            create: (context) => SearchBloc(),
+          ),
+          BlocProvider<SectorPerformanceBloc>(
+            create: (context) => SectorPerformanceBloc(),
+          ),
+          BlocProvider<NewsBloc>(
+            create: (context) => NewsBloc(),
+          ),
+          BlocProvider<TradesBloc>(
+            create: (context) => TradesBloc(),
+          ),
+        ],
+        child: BlocBuilder <SettingsBloc, SettingsState> (
+          builder: (context, state) {
+            return MaterialApp(
+              locale: DevicePreview.of(context).locale,
+              builder: DevicePreview.appBuilder,
+              title: 'Stonks',
+              theme: state.isNight ? darkMode : lightMode,
+              home: StockMarketAppHome(),
+              debugShowCheckedModeBanner: true,
+              routes: {
+                '/about': (context) => AboutSection(),
+              },
+            );
+          }
+        )
+      ),
     )
   );
 }
